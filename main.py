6346841872e4ee74695620939a4a873c15bf3bb2
pww217 <pwilson@anaconda.com>
2023-08-19 17:58:15 -0500

    working one time and continuous scheduling

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from kasa import SmartBulb
 from pprint import pprint
 
 ## Logging Configuration
-LOG_LEVEL = logging.INFO
+LOG_LEVEL = logging.DEBUG
 # Main module logger
 logger = logging.getLogger(__name__)
 logger.setLevel(LOG_LEVEL)
@@ -21,6 +21,7 @@ schedule_logger = logging.getLogger("schedule")
 schedule_logger.setLevel(LOG_LEVEL)
 schedule_logger.addHandler(sh)
 
+# Config
 def read_config(filename):
     # New keys can be added here
     keys = ["Bulbs", "Colors", "Routines", "Schedules"]
@@ -30,18 +31,20 @@ def read_config(filename):
     DEVICE_IPS, COLOR_VALUES, ROUTINES, SCHEDULES = [output.get(k) for k in keys]
     return DEVICE_IPS, COLOR_VALUES, SCHEDULES, ROUTINES
 
-def schedule_routine(routine):
+# Scheduling
+def schedule_continuous_routines(routine):
     start = SCHEDULES[routine["Schedule"]]["Start"]
     end   = SCHEDULES[routine["Schedule"]]["End"]
-    if end != None: # For continuous jobs
-        delta = datetime.strptime(end, "%H:%M") - datetime.strptime(start, "%H:%M")
-        if delta < timedelta(): # Find future time if delta returns past one
-            delta = delta + timedelta(days=1)
-        schedule.every().day.at(start).until(delta).do(execute_routine, routine=routine)
-        logger.debug(f"{routine} Start: {start}; End: {end}, Delta {delta}")
-    else: # For one-time jobs
-        schedule.every().day.at(start).do(execute_routine, routine=routine)
-        logger.debug(f"{routine} Start: {start}")
+    delta = datetime.strptime(end, "%H:%M") - datetime.strptime(start, "%H:%M")
+    logger.debug(f"{routine} Start: {start}; End: {end}, Delta {delta}")
+    if delta < timedelta(): # Find future time if delta returns past one
+        delta = delta + timedelta(days=1)
+    schedule.every().day.at(start).until(delta).do(execute_routine, routine=routine)
+
+def schedule_onetime_routines(routine):
+    start = SCHEDULES[routine["Schedule"]]["Start"]
+    schedule.every().day.at(start).do(execute_routine, routine=routine)
+    logger.debug(f"{routine} Start: {start}")
 
 def execute_routine(routine):
     devices = routine.get("Devices")
@@ -51,6 +54,7 @@ def execute_routine(routine):
     loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.gather(*calls))
 
+# API Calls
 async def call_api(routine, device):
     call = routine["Devices"][device]
     type, colors, brightness, interval = [call[k] for k in ["Type", "Colors", "Brightness", "Interval"]]
@@ -61,27 +65,32 @@ async def call_api(routine, device):
         case "power_on":
             await b.set_brightness(brightness)
             await b.turn_on(transition=transition)
+            logger.debug(f" POST {device}@{DEVICE_IPS[device]} | Turn On | Brightness: {brightness}; Interval: {interval}\n")
         case "power_off":
             await b.turn_off(transition=transition)
+            logger.debug(f" POST {device}@{DEVICE_IPS[device]} | Turn Off | Interval: {interval}\n")
         case "smooth_rotate":
             for c in colors:
                 hue, sat = COLOR_VALUES[c]
                 val = brightness
                 await b.set_hsv(hue, sat, val, transition=transition)
                 await asyncio.sleep(interval)
-    logger.debug(f" POST {device}@{DEVICE_IPS[device]} | Color: {c}; Brightness: {brightness}; Interval: {interval}\n")
-
+            logger.debug(f" POST {device}@{DEVICE_IPS[device]} | Rotate | Color: {c}; Brightness: {brightness}; Interval: {interval}\n")
 # Globals from config
 DEVICE_IPS, COLOR_VALUES, SCHEDULES, ROUTINES = read_config("config.yaml")
 
 def main():
+    for r in ROUTINES:
+        if SCHEDULES[r["Schedule"]]["End"] == None:
+            schedule_onetime_routines(r)
     while True:
         for r in ROUTINES:
-            schedule_routine(r)
-        logger.debug(f"Next run in {round(schedule.idle_seconds())} seconds")
+            if SCHEDULES[r["Schedule"]]["End"] != None:
+                schedule_continuous_routines(r)
+        logger.info(f"Next run in {round(schedule.idle_seconds())} seconds\n")
         schedule.run_pending()
         time.sleep(1)
-        logger.info(schedule.get_jobs())
+        logger.info(f"{schedule.get_jobs()}\n")
 
 
 if __name__ == "__main__":
