68f84b17914c502186a914cab9d67d69244bb7f4
pww217 <pwilson@anaconda.com>
2023-08-27 21:50:13 -0500

    renaming, adding webhook dockerfile and ci

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY . .
 RUN python -m pip install --upgrade pip \
 && pip install -r requirements.txt
 
-CMD ["python", "controller.py"]
+CMD ["python", "main.py"]
--- webhook.py
+++ webhook.py
@@ -1,14 +1,13 @@
-from flask import Flask,request,json
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def receive_webhook():
     if request.method == "GET":
-        pass
+        pass 
     if request.method == "POST":
         data = request.json
         return data
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+app.run(host='0.0.0.0', debug=True)
